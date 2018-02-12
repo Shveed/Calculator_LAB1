d8b1b6ffa5fd04224fda15f261cc0cec7840e73c
Shvedkov Ivan <32375298+Shveed@users.noreply.github.com>
2018-02-12 20:46:23 +0300

    Create calculator.h

---
+++ calculator.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+#include <iostream>
+#include <math.h>
+
+void printresult(double c);
+double add(double a, double b);
+double substract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
+
+#endif
